5bc24db9f80b24e7a04aae8be978d442d6968bfa
Elizabeth Nguyen <saper209@yahoo.com>
2022-04-05 16:10:18 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivArr, match){
+    const matchDriv = drivArr.filter(driver => driver.toLowerCase() === match.toLowerCase())
+    return matchDriv
+}
+
+function fuzzyMatch(drivArr, matchBegin){
+    function checkBeginning(driver){
+        return driver.slice(0, matchBegin.length) === matchBegin
+    }
+    const match = drivArr.filter(checkBeginning)
+    return match
+}
+
+function matchName(drivArr, name){
+    const match = drivArr.filter(driver => driver.name === name)
+    return match
+}
